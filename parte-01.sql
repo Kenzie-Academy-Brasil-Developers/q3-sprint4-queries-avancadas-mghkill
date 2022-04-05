d444bdf26ec80426eeec0df42fb1fd2b74a2e590
Filipe Melo <filipexpert63@gmail.com>
2022-04-05 14:41:50 -0300

    Configuração da parte 1 finalizada!

--- parte-01.sql
+++ parte-01.sql
@@ -1,25 +1,29 @@
 create table if not exists redes_sociais (
-	rede_social_id	SERIAL  	    primary key, 
-	nome 		    VARCHAR(150)    not null 		unique
+	id		SERIAL  	    primary key, 
+	nome 	VARCHAR(150)    not null 		unique
 );
 
 
 create table if not exists usuario_rede_sociais (
-	usuario_rede_id	SERIAL	    primary key, 
+	id				SERIAL	    primary key, 
 	usuario_id      INTEGER     not null,
-	rede_social_id  INTEGER     not null
+	rede_social_id  INTEGER     not null,
+	FOREIGN KEY (usuario_id) REFERENCES usuarios(id),
+	FOREIGN KEY (rede_social_id) REFERENCES redes_sociais(id)
 );
 
 create table if not exists usuarios (
-	usuario_id	SERIAL	    primary key,
+	id	SERIAL	    primary key,
 	nome 		VARCHAR(100),
 	email 		VARCHAR     not null unique,
 	senha 		VARCHAR     not null,
-	endereco_id INTEGER     not null
+	endereco_id INTEGER     not null,
+	FOREIGN KEY (endereco_id) REFERENCES enderecos(id)
+
 );
 
 create table if not exists enderecos (
-	endereco_id	SERIAL	        primary key,
+	id	SERIAL	        primary key,
 	rua         VARCHAR         not null,
 	pais        VARCHAR(100)    not NULL,
 	cidade      VARCHAR(100)    not NULL 
