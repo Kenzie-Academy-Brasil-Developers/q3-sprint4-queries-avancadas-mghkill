9e163feb7616ae8ab4f5e252dd47b7feb3a73d7a
Filipe Melo <filipexpert63@gmail.com>
2022-04-05 14:49:36 -0300

    Change at SQL01

--- parte-01.sql
+++ parte-01.sql
@@ -4,13 +4,13 @@ create table if not exists redes_sociais (
 );
 
 
-create table if not exists usuario_rede_sociais (
-	id				SERIAL	    primary key, 
-	usuario_id      INTEGER     not null,
-	rede_social_id  INTEGER     not null,
-	FOREIGN KEY (usuario_id) REFERENCES usuarios(id),
-	FOREIGN KEY (rede_social_id) REFERENCES redes_sociais(id)
+create table if not exists enderecos (
+	id	SERIAL	        primary key,
+	rua         VARCHAR         not null,
+	pais        VARCHAR(100)    not NULL,
+	cidade      VARCHAR(100)    not NULL 
 );
+
 
 create table if not exists usuarios (
 	id	SERIAL	    primary key,
@@ -22,10 +22,12 @@ create table if not exists usuarios (
 
 );
 
-create table if not exists enderecos (
-	id	SERIAL	        primary key,
-	rua         VARCHAR         not null,
-	pais        VARCHAR(100)    not NULL,
-	cidade      VARCHAR(100)    not NULL 
+
+create table if not exists usuario_rede_sociais (
+	id				SERIAL	    primary key, 
+	usuario_id      INTEGER     not null,
+	rede_social_id  INTEGER     not null,
+	FOREIGN KEY (usuario_id) REFERENCES usuarios(id),
+	FOREIGN KEY (rede_social_id) REFERENCES redes_sociais(id)
 );
-	
+
